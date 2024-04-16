eac3a0141de8d6ebc513a1a896761cfb732e881c
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-17 02:23:04 +0530

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -27,3 +27,15 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next)=>{
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message
+
+})
+});
