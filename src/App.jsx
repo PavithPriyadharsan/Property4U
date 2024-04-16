34559e2ca5ac5a40af53e88dcc163b6d975695ef
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-17 00:15:38 +0530

    create Header nav

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About';
 import Signin from './pages/SignIn';
 import Signup from './pages/SignUp';
 import Profile from './pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Signin />} />
