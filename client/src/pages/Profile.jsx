57947e2eb29acc77ca5a1468c2ad632d4ff5ba3e
PavithPriyadharsan <pavithpriyadarshan222@gmail.com>
2024-04-19 23:13:10 +0530

    create signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, signOutUserFailure, signOutUserSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
   const { currentUser, loading, error } = useSelector(state => state.user);
@@ -86,6 +86,21 @@ const handleDeleteUser = async () => {
   }
 }
 
+const handleSignOut = async () => {
+  try {
+    dispatch(signOutUserStart());
+    const res = await fetch('/api/auth/signout');
+    const data = await res.json();
+    if(data.success === false){
+      dispatch(signOutUserFailure(data.message));
+      return;
+    }
+    dispatch(signOutUserSuccess(data));
+  } catch (error) {
+    dispatch(signOutUserFailure(error.message));
+  }
+}
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-5'>Profile</h1>
@@ -133,7 +148,7 @@ const handleDeleteUser = async () => {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-500 cursor-pointer'>Delete Account</span> 
-        <span className='text-red-500 cursor-pointer'>Sign out</span> 
+        <span onClick={handleSignOut} className='text-red-500 cursor-pointer'>Sign out</span> 
       </div>
           <p className='text-red-700 mt-5'>{error ? error : ''}</p>
           <p className='text-green-500 mt-1'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
